bc78df1b73f4424607d44f7f8c480e14ad68ee70
YouMing <youming2@ualberta.ca>
2025-01-06 19:04:10 -0700

    tabs add, nav bar done

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,11 +1,71 @@
-import { View, Text } from "react-native";
-import { Tabs, Redirect } from "expo-router";
+import { View, Text, Image } from "react-native";
+import { Tabs } from "expo-router";
+import icons from "../../constants/icons"; // 使用默认导入
+
+const TabIcon = ({ icon, name, focused }) => {
+  const iconColor = focused ? "#6387A9" : "#A7BFD6";
+
+  return (
+    <View
+      className="items-center justify-center gap-2 top-6
+    "
+    >
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={iconColor}
+        className="h-7 w-7"
+      />
+      <Text
+        className={`${
+          focused ? "font-bold" : "font-light"
+        } text-xs text-center w-20`}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
 
 const TabLayout = () => {
   return (
     <>
-      <Tabs>
-        <Tabs.Screen name="home" />
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ focused, name }) => (
+              <TabIcon icon={icons.home} name="Dashboard" focused={focused} />
+            ), // 确保 icons.home 可用
+          }}
+        />
+
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Create",
+            headerShown: false,
+            tabBarIcon: ({ focused, name }) => (
+              <TabIcon icon={icons.plus} name="Add food" focused={focused} />
+            ), // 确保 icons.home 可用
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ focused, name }) => (
+              <TabIcon icon={icons.profile} name="PROFILE" focused={focused} />
+            ), // 确保 icons.home 可用
+          }}
+        />
       </Tabs>
     </>
   );
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet } from "react-native";
 import { Slot } from "expo-router";
 import React from "react";
+import "../global.css";
 
 const Layout = () => {
   return (
