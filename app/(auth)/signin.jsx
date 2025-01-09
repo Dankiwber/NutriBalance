0782e86bf6a2c13bfbbfd388cb237c313e802d11
YouMing <youming2@ualberta.ca>
2025-01-08 17:39:53 -0700

    signin done, database added

--- app/(auth)/signin.jsx
+++ app/(auth)/signin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Image,
   View,
@@ -12,9 +12,21 @@ import * as Yup from "yup";
 import { Link, useRouter } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import icons from "../../constants/icons";
-import { singIn } from "../../lib/appwrite";
+import { signIn, account } from "../../lib/appwrite";
 
 const LoginScreen = () => {
+  useEffect(() => {
+    const clearSessionsOnLoad = async () => {
+      try {
+        await account.deleteSessions(); // 清理所有活动会话
+      } catch (error) {
+        console.log("No active session to clear.", error);
+      }
+    };
+
+    clearSessionsOnLoad();
+  }, []); // 只在组件挂载时执行
+
   const router = useRouter();
   const [showPassword, setShowPassword] = useState(false);
 
@@ -29,10 +41,10 @@ const LoginScreen = () => {
 
   const handleLoginSubmit = async (values) => {
     try {
-      await singIn(values.email, values.password);
+      await signIn(values.email, values.password);
       router.replace("/home");
     } catch (error) {
-      alert("Invalid email or password");
+      alert("Error", error.message);
     }
   };
 
